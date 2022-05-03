7c8fd84dc8860e09568ae5e9584d9be4d15903d1
Emil Irgi <emilirgi18@gmail.com>
2022-05-03 09:28:17 +0700

    GET POST supplier jalan

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const passport = require("passport");
+const bodyParser = require('body-parser')
+
 
 const sessionConf = require("./conf/session.conf");
 require("./conf/passport.conf");
@@ -10,13 +12,15 @@ const corsConf = require("./conf/cors.conf");
 
 const authRouter = require("./routes/auth.router");
 const userRouter = require("./routes/user.router");
+const supplierRouter = require("./routes/supplier.router");
 
 const { isAuth } = require("./middleware");
 
 const app = express();
 
 app.use(morgan("dev"));
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(corsConf);
 
 // express-session middleware
@@ -29,5 +33,6 @@ app.use("/auth/google", authRouter);
 
 // protected routes
 app.use("/api/user", isAuth, userRouter);
+app.use("/api/supplier", isAuth, supplierRouter);
 
 app.listen(5000, () => console.log("http://localhost:5000"));

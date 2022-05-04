f989538736950e7260270ab6a7cd48cf453630e8
theogee <theophilusgee17@gmail.com>
2022-05-04 16:48:56 +0700

    changed safety_stock_qty data type from int to real to allow floating number

--- postgre/erp-project-ddl.sql
+++ postgre/erp-project-ddl.sql
@@ -4,7 +4,7 @@ create table users (
 	firstname varchar(30) not null,
 	lastname varchar(30) not null,
 	profile_picture varchar(200)
-)
+);
 
 create table business (
 	business_id serial primary key,
@@ -14,26 +14,26 @@ create table business (
 	constraint fk_user
 		foreign key(user_id)
 			references users(user_id)
-)
+);
 
 create table measurement (
 	measurement_id serial primary key,
 	name varchar(10) not null
-)
+);
 
 create table material (
 	material_id serial primary key,
 	business_id int not null,
 	measurement_id int not null,
 	name varchar not null,
-	safety_stock_qty int,
+	safety_stock_qty real,
 	constraint fk_business
 		foreign key(business_id)
 			references business(business_id),
 	constraint fk_measurement
 		foreign key (measurement_id)
 			references measurement(measurement_id)
-)
+);
 
 create table supplier (
 	supplier_id serial primary key,
@@ -44,7 +44,7 @@ create table supplier (
 	constraint fk_business
 		foreign key(business_id)
 			references business(business_id)
-)
+);
 
 create table batches (
 	batch_id serial primary key,
@@ -62,5 +62,5 @@ create table batches (
 	constraint fk_supplier
 		foreign key (supplier_id)
 			references supplier(supplier_id)
-)
+);
 

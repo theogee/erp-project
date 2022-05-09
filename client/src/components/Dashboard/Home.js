7a02c95a0936ba5edcc09b54ab62f373a5b7366e
theogee <theophilusgee17@gmail.com>
2022-05-09 16:49:43 +0700

    removed console.log user from Home

--- client/src/components/Dashboard/Home.js
+++ client/src/components/Dashboard/Home.js
@@ -4,7 +4,6 @@ import { useOutletContext } from "react-router-dom";
 
 export default function Home() {
   const user = useOutletContext();
-  console.log(user);
   return (
     <section className="business">
       <h1>Hello, {user.firstname + " " + user.lastname}!</h1>

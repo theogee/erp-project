88bd5897c944d70b26ad05781ac8e43be74ce622
theogee <theophilusgee17@gmail.com>
2022-05-09 15:40:25 +0700

    styled & working sidenav (react-router + emotion + mui)
    problem with how messy the file is, since it combines css, jsx, and js. Probably need to split into multiple files?

--- client/src/components/Dashboard/SideNav.js
+++ client/src/components/Dashboard/SideNav.js
@@ -1,19 +1,37 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import { jsx } from "@emotion/react";
 import axios from "axios";
 
 import { NavLink, useNavigate } from "react-router-dom";
+
+import styled from "@emotion/styled";
 
 import Stack from "@mui/material/Stack";
 import Link from "@mui/material/Link";
 import Avatar from "@mui/material/Avatar";
 
+const StyledStack = styled(Stack)`
+  background-color: white;
+  width: 260px;
+  padding: 24px;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const StyledLink = styled(Link)`
+  padding: 12px;
+  font-size: 14px;
+  border-radius: 4px;
+`;
+
+const StyledGreyLink = styled(Link)`
+  padding: 12px;
+  font-size: 12px;
+  color: #4b5563;
+`;
+
 export default function SideNav(props) {
   const SERVER_URL = process.env.REACT_APP_SERVER_URL;
   const navigate = useNavigate();
-
-  const style = {
-    width: "260px",
-  };
 
   const logout = async () => {
     try {
@@ -26,26 +44,68 @@ export default function SideNav(props) {
     }
   };
 
+  const links = [{ to: "/dashboard", name: "home" }];
+  const LinksComponent = () => {
+    return links.map((link) => (
+      <StyledLink
+        key={link.name}
+        component={NavLink}
+        to={link.to}
+        underline="none"
+        style={({ isActive }) => ({
+          backgroundColor: isActive ? "black" : "white",
+          color: isActive ? "white" : "black",
+        })}
+      >
+        {link.name}
+      </StyledLink>
+    ));
+  };
+
+  const greyLinks = [
+    { to: "/dashboard/settings", name: "Settings" },
+    { to: "/", name: "Logout", onClick: logout },
+  ];
+  const GreyLinksComponent = () => {
+    return (
+      <Stack sx={{ marginTop: "24px" }}>
+        {greyLinks.map((link) => (
+          <StyledGreyLink
+            key={link.name}
+            component={NavLink}
+            to={link.to}
+            underline="none"
+            onClick={link.onClick ? link.onClick : ""}
+          >
+            {link.name}
+          </StyledGreyLink>
+        ))}
+      </Stack>
+    );
+  };
+
   return (
     <nav className="sidebar">
-      <Stack sx={style}>
-        <div className="profile">
-          <p style={{ float: "right" }}>
-            {props.firstname + " " + props.lastname}
-          </p>
-          <p style={{ float: "right" }}>{props.email}</p>
+      <StyledStack sx={{ height: "100vh" }}>
+        <Stack
+          direction="row"
+          alignItems="center"
+          sx={{ marginBottom: "24px" }}
+        >
           <Avatar
             alt={props.firstname + " " + props.lastname}
             src={props.profile_picture}
           />
-        </div>
-        <Link component={NavLink} to="/dashboard/home">
-          home
-        </Link>
-        <Link component={NavLink} to="/" onClick={logout}>
-          Logout
-        </Link>
-      </Stack>
+          <div css={{ marginLeft: 8 }}>
+            <p css={{ fontWeight: 500, fontSize: 14 }}>
+              {props.firstname + " " + props.lastname}
+            </p>
+            <p css={{ fontSize: 12 }}>{props.email}</p>
+          </div>
+        </Stack>
+        <LinksComponent />
+        <GreyLinksComponent />
+      </StyledStack>
     </nav>
   );
 }

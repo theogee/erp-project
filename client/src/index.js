54132fb3a8664196016db90e22a0fc3cf5bfcd48
theogee <theophilusgee17@gmail.com>
2022-06-08 11:09:43 +0700

    solved missed conflict

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
->>>>>>> 7d3ebf9319798bae615e2eeadad1e0cbe452f3f0
 
 import "./index.css";
 // custom MUI theme
@@ -14,15 +10,10 @@ import { ThemeProvider } from "@mui/material/styles";
 
 // Pages
 import App from "./App";
-<<<<<<< HEAD
-import NotFound from "./components/NotFound";
-import Login from "./components/Login";
-=======
 import NotFound from "./pages/NotFound";
-import Homepage from './pages/Home';
-import Pricing from './pages/PricingPage';
-import Login from './components/Form/LoginPage';
->>>>>>> 7d3ebf9319798bae615e2eeadad1e0cbe452f3f0
+import Homepage from "./pages/Home";
+import Pricing from "./pages/PricingPage";
+import Login from "./components/Form/LoginPage";
 
 import {
   Dashboard,
@@ -51,11 +42,11 @@ ReactDOM.render(
   <ThemeProvider theme={theme}>
     <BrowserRouter>
       <Routes>
-	  	  <Route path="/" element={<App />}>
-           <Route path="home" element={<Homepage />} />
-           <Route path="pricing" element={<Pricing />} />
-		       <Route path="login" element={<Login />} />
-		       <Route index element={<Navigate replace to="/home" />} />
+        <Route path="/" element={<App />}>
+          <Route path="home" element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route index element={<Navigate replace to="/home" />} />
         </Route>
         <Route path="/dashboard" element={<Dashboard />}>
           <Route path="business" element={<Business />} />
